e1d1d92c632b452dbba254c41c2df7f0cb33cb01
Luuk <luukbrauckmann@gmail.com>
2023-06-12 15:48:10 +0200

    Create new request form

--- src/routes/new-request.page.js
+++ src/routes/new-request.page.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { create } from "../lib/data-access.js"
 
 const newRequestPage = express.Router()
 
@@ -6,11 +7,18 @@ const options = {
 	path: '/demo-aanvragen',
 	title: 'Demo aanvragen',
 	template: 'new-request.ejs',
-	styles: [],
+	styles: ['/pages/new-request.css', '/components/form.css'],
 	scripts: [],
 	hostClass: 'new-request'
 }
 
 newRequestPage.get(options.path, async (request, response) => response.render('index', options))
+newRequestPage.post(options.path, async (request, response) => {
+	const { body } = request
+	const createResponse = create('', body)
+	createResponse.then((result) => console.log(result))
+	createResponse.catch((result) => console.log(result))
+	// response.render('index', options)
+})
 
 export default newRequestPage
